4d34bb15f26ef7d4089a0e6b1c1ef2d3441c0416
Zachary Smith <z.smith32@gmail.com>
2017-07-08 22:26:22 -0700

    increment routine index function

--- Enemy.cpp
+++ Enemy.cpp
@@ -1,27 +1,21 @@
 #include <Enemy.hpp>
 
-float i = 0.25;
-float Enemy::_xMoveList[16] = {0,0,0,0, i,i,i,i, 0,0,0,0, -i,-i,-i,-i};
-float Enemy::_yMoveList[16] = {i,i,i,i, 0,0,0,0, i,i,i,i,  0,0,0,0};
+float Enemy::_xRoutine[4] = {0, 4, 0, -4};
+float Enemy::_yRoutine[4] = {4, 0, 4, 0};
 
-// float Enemy::_xMoveList[64] = {
-// 								0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-// 								i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i, 
-// 								0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 
-// 								-i,-i,-i,-i,-i,-i,-i,-i,-i,-i,-i,-i,-i,-i,-i,-i
-// 								};
-// float Enemy::_yMoveList[64] = {	i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i, 
-// 								0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 
-// 								i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i, 
-// 								0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-// 								};
 Enemy::Enemy(int x, int y, float speed) {
 	xpos = x;
 	ypos = y;
+	xwid = 3;
+	ywid = 3;
+	_xCurr = 0;
+	_yCurr = 0;
+	sprite = " ^ < > v ";
 	symbol = '+';
 
-	_moveIndex = 0;
-	_moveIndexMax = 4 * 16;
+	_speed = speed;
+	_routineIndex = 0;
+	_routineIndexMax = 3;
 	return ;
 }
 
@@ -36,13 +30,79 @@ Enemy & Enemy::operator=(Enemy & src) {
 	return (*this);
 }
 
-void	Enemy::move() {
-	moveX(_xMoveList[_moveIndex] * _speed);
-	moveY(_yMoveList[_moveIndex] * _speed);
+void Enemy::draw()
+{
+	draw_sprite();
+}
 
-	if (_moveIndex == _moveIndexMax - 1)
-		_moveIndex = 0;
+void	Enemy::move() {
+	incrementRoutine();
+	moveX(_xRoutine[_routineIndex]);
+	moveY(_yRoutine[_routineIndex]);
+
+	if (_routineIndex == _routineIndexMax - 1)
+		_routineIndex = 0;
 	else
-		_moveIndex++;
+		_routineIndex++;
 	return ;
 }
+
+void	Enemy::incrementRoutine(void) {
+	int		xNeg;
+	int		yNeg;
+	float	tXCurr = _xCurr;
+	float	tXMax = _xRoutine[_routineIndex];
+	float	tYCurr = _yCurr;
+	float	tYMax = _yRoutine[_routineIndex];
+
+	// change to positive
+	if (tXMax < 0) {
+		xNeg = 1;
+		tXCurr *= -1;
+		tXMax *= -1;
+	}
+	if (tYMax < 0) {
+		yNeg = 1;
+		tYCurr *= -1;
+		tYMax *= -1;
+	}
+	// see if curr has reached script val
+	if (tXCurr >= tXMax || tYCurr >= tYMax) {
+		if (_routineIndex == _routineIndexMax)
+			_routineIndex++;
+		else
+			_routineIndex = 0;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Enemy.hpp
+++ Enemy.hpp
@@ -3,21 +3,27 @@
 
 #include <iostream>
 #include "Drawable.hpp"
+#include <cmath>
 
 class Enemy : public Drawable {
 
 	public:
-		Enemy(int xpos, int ypos);
+		Enemy(int xpos, int ypos, float speed);
 		~Enemy(void);
 		Enemy & operator=(Enemy & src);
+		void incrementRoutine(void);
 		void move();
+		void draw();
 
 	private:
 		float _speed;
-		int	_moveIndex;
-		int	_moveIndexMax;
-		static float _xMoveList[64];
-		static float _yMoveList[64];
+		int	_routineIndex;
+		int	_routineIndexMax;
+		static float _xRoutine[4];
+		static float _yRoutine[4];
+
+		float _xCurr;
+		float _yCurr;
 
 };
 
