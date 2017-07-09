6c3e61a0ebb1b5202947dba5c12b2b01ab5c6ac9
Phil MCLAUGHLIN <pmclaugh@student.42.us.org>
2017-07-08 17:28:29 -0700

    restored drawable.hpp

--- Drawable.hpp
+++ Drawable.hpp
@@ -3,4 +3,24 @@
 
 #include <iostream>
 
+class Drawable{
+public:
+	Drawable();
+	int getX();
+	int getY();
+	void moveX(int delta);
+	void moveY(int delta);
+	void move();
+	void draw();
+	Drawable* getNext();
+	static void draw_all();
+	static void move_all();
+protected:
+	int xpos;
+	int ypos;
+	char symbol;
+	static Drawable* list;
+	Drawable* next;
+};
+
 #endif
